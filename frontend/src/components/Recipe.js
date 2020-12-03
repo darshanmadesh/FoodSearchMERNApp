714ebcc6a8107b06fc8500e89798a49bbea9652b
Darshan Madesh <darshan.madesh@gmail.com>
2020-12-03 14:01:08 +0530

    Added ReactRouter and ReactRouterBootstrap

--- frontend/src/components/Recipe.js
+++ frontend/src/components/Recipe.js
@@ -1,19 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Card } from "react-bootstrap";
 import Rating from "./Rating";
 
 const Recipe = ({ recipe }) => {
     return (
         <Card className='my-3 p-3 rounded'>
-            <a href={`/recipe/${recipe.Name}`}>
+            <Link to={`/recipe/${recipe.Name}`}>
                 <Card.Img src={recipe.Image} variant='top' style={{ height: "100%" }} />
-            </a>
+            </Link>
+
             <Card.Body>
-                <a href={`/recipe/${recipe.Name}`}>
-                    <Card.Title as='div'>
+                <Link to={`/recipe/${recipe.Name}`}>
+                    <Card.Title as='h4'>
                         <strong>{recipe.Name}</strong>
                     </Card.Title>
-                </a>
+                </Link>
 
                 <Card.Text as='div'>
                     <Rating value={4.5} text={`  50 reviews`} color='gold' />
---
+++ frontend/src/screens/RecipeScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RecipeScreen = () => {
+    return <div>Recipe</div>;
+};
+
+export default RecipeScreen;
